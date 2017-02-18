b017ee723a671fd7c861445a6934f22958867617
martinchu <martinchu09@gmail.com>
2017-02-18 13:39:17 +0000

    Add new page (projects)

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,8 +2,11 @@ function myFunction() {
    document.getElementById("test").innerHTML = "Paragraph changed.";
 }
 
-function reponSquare(){
+function responSquare(){
   // document.getElementByClassName("WallPhoto")
+  var w = window.outerWidth;
+  document.getElementByClassName("grid").style.width = w/4 + 'px';
+  document.getElementByClassName("grid").style.width = w/4 + 'px';
 }
 document.getElementById("HTMLBody").onload = function(){beforeLoad()};
 function beforeLoad(){
