d4870e7842b135792fbb9047b8d746bf28c25843
martinchu <martinchu09@gmail.com>
2017-02-21 15:33:00 +0000

    Push Testing

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,8 +1,3 @@
-
-function myFunction() {
-  document.getElementById("test").innerHTML = "Paragraph changed.";
-}
-
 function responSquare(){
   // document.getElementByClassName("WallPhoto")
   var cn = "gridV2";
@@ -25,30 +20,25 @@ function responSquare(){
     photos[i].style.height = newwidth + 'px';
     var dummy = photos[i].getElementsByTagName('img').length;
     // for(var j = 0; j < length; i++){
-    var childimg = photos[i].getElementsByTagName('img')[0];
-    var imgwidth = childimg.clientWidth;
-    var imgheight= childimg.clientHeight;
-    if(w >= 750){
-      if(imgwidth > imgheight){
-        childimg.style.height = newwidth + 'px';
-        childimg.style.width = newwidth * imgwidth/imgheight + 'px';
-        childimg.style.position = "absolute";
-        // childimg.style.marginLeft =
-      }
-      else{
+    var childimg = photos[i].getElementsByTagName('img')[0]
+    var imgwidth = childimg.clientWidth
+    var imgheight = childimg.clientHeight
 
-      }
+    if(imgwidth > imgheight){
+      childimg.style.height = newwidth + 'px'
+      childimg.style.width = newwidth * imgwidth / imgheight + 'px'
+      childimg.style.position = 'absolute'
+      // childimg.style.marginLeft =
+    } else {
+      childimg.style.width = newwidth + 'px'
     }
-    else{
-      childimg.style.width = newwidth + 'px';
-      childimg.style.height = imgheight + 'px';
-    }
+
     // document.getElementsByClassName("grid")[i].setAttribute("style","width:" + text);
     // document.getElementsByClassName("grid")[i].setAttribute("style","height:"+ text);
   }
-  console.log("imgwidth = "+ imgwidth);
-  console.log("imgheight = "+ imgheight);
-  console.log("w: "+w);
+  console.log('imgwidth = ' + imgwidth)
+  console.log('imgheight = ' + imgheight)
+  console.log('w: ' + w)
   return text;
 }
 // document.getElementById("HTMLBody").onload = function(){beforeLoad()};
