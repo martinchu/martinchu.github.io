8647997e287d70ce6873681fb69a872af2cb4494
Martin Chu <martinchu09@gmail.com>
2017-03-10 18:07:52 +0000

    new jekyll build;

--- _site/Scripts/main.js
+++ _site/Scripts/main.js
@@ -1,3 +1,73 @@
-function myFunction() {
-   document.getElementById("test").innerHTML = "Paragraph changed.";
+function myMap() {
+  var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:3,
+  };
+  var map=new google.maps.Map(document.getElementById("GoogleMap"),mapProp);
+  var myCenter = new google.maps.LatLng(51.508742,-0.120850);
+  var image = {
+    url:'logo-black-small.png',
+    size: new google.maps.Size(50, 50),
+    origin: new google.maps.Point(0, 0)
+  }
+  var marker = new google.maps.Marker({
+    position: myCenter
+    // animation:google.maps.Animation.BOUNCE,
+    // icon: image
+  });
+  marker.setMap(map);
+  var currentLocation = new google.maps.InfoWindow({
+    content: "My Current Location"
+  });
+  currentLocation.open(map,marker);
 }
+
+
+
+function responSquare(){
+  // document.getElementByClassName("WallPhoto")
+  var cn = "grid";
+  var col = 4;
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  var photos = document.getElementsByClassName(cn);
+  var length = photos.length;
+  var text = "";
+  var newwidth;
+  if(w >= 750){
+    newwidth = w/col;
+  }
+  else{
+    newwidth = w;
+  }
+
+  for(var i = 0; i < length; i++){
+    photos[i].style.width = newwidth + 'px';
+    photos[i].style.height = newwidth + 'px';
+    var dummy = photos[i].getElementsByTagName('img').length;
+    // for(var j = 0; j < length; i++){
+    var childimg = photos[i].getElementsByTagName('img')[0]
+    var imgwidth = childimg.clientWidth
+    var imgheight = childimg.clientHeight
+
+    if(imgwidth > imgheight){
+      childimg.style.height = newwidth + 'px'
+      childimg.style.width = newwidth * imgwidth / imgheight + 'px'
+      childimg.style.position = 'absolute'
+      // childimg.style.marginLeft =
+    } else {
+      childimg.style.width = newwidth + 'px'
+    }
+
+    // document.getElementsByClassName("grid")[i].setAttribute("style","width:" + text);
+    // document.getElementsByClassName("grid")[i].setAttribute("style","height:"+ text);
+  }
+  console.log('imgwidth = ' + imgwidth)
+  console.log('imgheight = ' + imgheight)
+  console.log('w: ' + w)
+  return text;
+}
+// document.getElementById("HTMLBody").onload = function(){beforeLoad()};
+// function beforeLoad(){
+//
+// }
