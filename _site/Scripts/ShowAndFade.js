4d9e02aec0fa0188a3718331a6f4d15e23940ed8
Martin Chu <martinchu09@gmail.com>
2017-03-18 19:28:10 +0000

    new build

--- _site/Scripts/ShowAndFade.js
+++ _site/Scripts/ShowAndFade.js
@@ -3,7 +3,11 @@ $(function(){
     $(".gridV2").css('display','inline-block');
     console.log("inline-block display done after delay!")
     console.log("change?")
-  }, 10000);
+  }, 5000);
+  setTimeout(function(){
+    $(".gridV2").css('visibility','visible');
+    console.log("inline-block display done after delay!")
+    console.log("change?")
+  }, 5000);
 
-  $(".gridV2").css('visibility','hidden');
 });
