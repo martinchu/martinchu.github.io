436384b9b13ceefc17164a48bd99000c4f1972a0
Martin Chu <martinchu09@gmail.com>
2017-04-02 17:30:43 +0100

    new html content in sort.js

--- Scripts/sort.js
+++ Scripts/sort.js
@@ -7,10 +7,12 @@ function SortCategory(){
   document.getElementById("SkillCategory").disabled = true;
   console.log("SC")
   // $(".")
+  $("#Line1").html("Line1 Sort by Category")
+
 }
 function SortProficiency(){
   document.getElementById("SkillLevel").disabled = true;
   document.getElementById("SkillCategory").disabled = false;
   console.log("SP")
-
+  $("#Line1").html("Line1 Sort by Proficiency")
 }
