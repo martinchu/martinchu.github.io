75cf6441788dd630eb4857a5b6c2e6cce23070ac
Martin Chu <martinchu09@gmail.com>
2017-04-06 18:22:19 +0100

    fix global header issue

--- _site/Scripts/sort.js
+++ _site/Scripts/sort.js
@@ -1,5 +1,8 @@
-function Line1(callback){
-
+function cbContentChange (tag, newContent ,callback){
+  callback(tag,newContent)
+}
+function ContentChange(tag, newContent){
+  $(tag).html(newContent)
 }
 
 function SortCategory(){
@@ -7,12 +10,46 @@ function SortCategory(){
   document.getElementById("SkillCategory").disabled = true;
   console.log("SC")
   // $(".")
+  var subtitle =['Backend','Web','Others']
+  var language =['C++, C, Java, Python']
   $("#Line1").html("Line1 Sort by Category")
-
+  // Fadeout Line1 -> ChangeLine1 -> FadeInLine1
+  for(i = 0; i<subtitle.length;i++){
+    cbContentChange(subtitle[i],language[i],myFadeOhut)
+  }
 }
 function SortProficiency(){
   document.getElementById("SkillLevel").disabled = true;
   document.getElementById("SkillCategory").disabled = false;
   console.log("SP")
   $("#Line1").html("Line1 Sort by Proficiency")
+
 }
+function Transition(num,ContentArray,callback){
+  $("line"+num).fadeOut(1000).html(ContentArray[num]).fadein(1000)
+  if(num<ContentArray.length){
+    callback(num+1,ContentArray,callback);
+  }
+}
+
+/*
+DRAFT:
+
+
+
+$(Button1).out(1000).html("dafsn").in(1000)
+
+$(button1).fadeout(1000, function(){
+
+});
+
+$('#join').fadeOut(400, function() {
+$(this).html('Success').fadeIn(3000, function() {
+$(this).fadeOut(3000, function(){
+$(this).html('Sign UP').fadeIn();
+})
+})
+});
+
+
+*/
